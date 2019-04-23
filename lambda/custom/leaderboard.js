d5462a8fd35f9f47a490ca67fc26e1906584e047
scorchio36 <christianhecker36@yahoo.com>
2019-04-22 19:23:12 -0500

    Fix new highscore leaderboard bug.
    When a new high score was a achieved it would
    replace one of the old score entries. Now the
    new score will be inserted between old entries
    and the lower entries will be scooted down. The
    last entry will be popped in order to maintain
    the desired length of the leaderboard.

--- lambda/custom/leaderboard.js
+++ lambda/custom/leaderboard.js
@@ -1,5 +1,5 @@
-const NAME_INDEX = 0;
-const SCORE_INDEX = 1;
+const NAME_INDEX = 0; //name is stored in index 0 in the board array
+const SCORE_INDEX = 1; //score is stored in index 1 of the board array
 
 function Leaderboard(length, object) {
 
@@ -38,13 +38,20 @@ Leaderboard.prototype.isScoreHighEnough = function(score) {
 }
 
 //add a name to a certain position
+//Do not call this before addScoreToPosition has been called. This method
+//must be called after because it depends on the splice done in the
+//addScoreToPosition method.
 Leaderboard.prototype.addNameToPosition = function(name, position) {
   this.board[position][NAME_INDEX] = name;
 }
 
 //add a score to a certain position
+//Splice is used to insert a new entry in between the current entries
+//rather than just writing over past entries.
 Leaderboard.prototype.addScoreToPosition = function(score, position) {
-  this.board[position][SCORE_INDEX] = score;
+  //this.board[position][SCORE_INDEX] = score;
+  this.board.splice(position, 0, [this.board[position][NAME_INDEX], score]);
+  this.board.pop(); //We want to maintain the desired length of the board
 }
 
 Leaderboard.prototype.getNameFromPosition = function(position) {
